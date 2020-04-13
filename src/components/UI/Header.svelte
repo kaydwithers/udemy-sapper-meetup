6d654b5d418ead6e98ffcb302d2c8c747d9ccaad
Kayd Withers <kayd.withers@isobar.com>
2020-04-13 16:06:13 +1000

    preview

--- src/components/UI/Header.svelte
+++ src/components/UI/Header.svelte
@@ -2,20 +2,23 @@
   import Heading from "./Heading.svelte";
   import Layout from "./Layout.svelte";
   import LayoutItem from "./LayoutItem.svelte";
+  import Page from "./Page.svelte";
 </script>
 
 <header class="bg-violet">
-  <Layout 
-    py="py-4" 
-    class="items-center"
-  >
-    <LayoutItem class="text-center">
-      <Heading 
-        size="1"
-        spacing="mb-0"
-      >
-        Kayd&rsquo;s Knowledge Memorandum 📚
-      </Heading>
-    </LayoutItem>
-  </Layout>
+  <Page>
+    <Layout 
+      py="py-4" 
+      class="items-center"
+    >
+      <LayoutItem class="text-center">
+        <Heading 
+          size="1"
+          spacing="mb-0"
+        >
+          Kayd&rsquo;s Knowledge Memorandum 📚
+        </Heading>
+      </LayoutItem>
+    </Layout>
+  </Page>
 </header>
